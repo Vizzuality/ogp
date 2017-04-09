277955afb37a947b584e5e0f0ce7b5133ccdde36
Hector Vizzuality <hectorvizzuality@MacBook-Air-de-Hector.local>
2017-04-10 00:31:20 +0200

    change tabs styles on working group pages

--- sites/all/themes/custom/ogp_theme/templates/node--working-group.tpl.php
+++ sites/all/themes/custom/ogp_theme/templates/node--working-group.tpl.php
@@ -17,41 +17,35 @@ $attachments = field_get_items('node', $node, 'field_attachment');
     </div>
   </section>
 
-  <section class="working-group-content">
+  <section class="row working-group-content">
     <div class="c-loader"><div class="loader"></div></div>
-    <div class="c-tabs -horizontal">
-      <div class="tabs">
-        <div class="row">
-          <div class="column small-12">
-            <ul class="tabs-container">
-              <li class="tab text -tab" data-node="about">About</li>
-            </ul>
-          </div>
-        </div>
+    <div class="column small-12 medium-3">
+      <div class="c-tabs -vertical">
+        <ul class="tabs-container">
+          <li class="tab text -tab -selected" data-node="about">About</li>
+        </ul>
       </div>
     </div>
-    <div class="row">
-      <div class="column small-12 medium-8 medium-offset-2" id="container-info">
-        <div class="tab-content about">
-          <h3 class="text -section-title">About</h3>
+    <div class="small-8" id="container-info">
+      <div class="tab-content about">
+        <h3 class="text -section-title">About</h3>
+        <div class="text -body-content">
+          <?php echo $node->body[$node->language][0]['value']; ?>
+        </div>
+      </div>
+      <?php if ($attachments) { ?>
+        <div class="c-documents">
+          <h1 class="text -section-title">Documents</h1>
           <div class="text -body-content">
-            <?php echo $node->body[$node->language][0]['value']; ?>
+            <?php foreach ($attachments as $attachment) { ?>
+              <div class="c-link">
+                <svg class="icon -blue -small"><use xlink:href="#icon-external-link"></use></svg>
+                <a class="text -link -blue" href="<?php echo file_create_url($attachment['uri']) ?>" target="_blank" rel="nofollow"><?php echo $attachment['filename'] ?></a>
+              </div>
+            <?php } ?>
           </div>
         </div>
-        <?php if ($attachments) { ?>
-          <div class="c-documents">
-            <h1 class="text -section-title">Documents</h1>
-            <div class="text -body-content">
-              <?php foreach ($attachments as $attachment) { ?>
-                <div class="c-link">
-                  <svg class="icon -blue -small"><use xlink:href="#icon-external-link"></use></svg>
-                  <a class="text -link -blue" href="<?php echo file_create_url($attachment['uri']) ?>" target="_blank" rel="nofollow"><?php echo $attachment['filename'] ?></a>
-                </div>
-              <?php } ?>
-            </div>
-          </div>
-        <?php } ?>
-      </div>
+      <?php } ?>
     </div>
   </section>
 
