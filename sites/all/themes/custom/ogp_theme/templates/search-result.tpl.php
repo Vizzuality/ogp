acadca3957783b651e621873cb3f32bf23563a09
Edward Brett <e.j.a.brett@gmail.com>
2017-04-20 11:10:17 +0200

    fix formating of search summary

--- sites/all/themes/custom/ogp_theme/templates/search-result.tpl.php
+++ sites/all/themes/custom/ogp_theme/templates/search-result.tpl.php
@@ -63,7 +63,13 @@
  *
  * @ingroup themeable
  */
- $snippet = substr($result['node']->body['und'][0]['value'], 0, 100);
+ $limit = 12;
+ $snippet = strip_tags($result['node']->body['und'][0]['value']);
+ if (str_word_count($snippet, 0) > $limit) {
+     $words = str_word_count($snippet, 2);
+     $pos = array_keys($words);
+     $snippet = substr($snippet, 0, $pos[$limit]) . '...';
+ }
 ?>
 <li class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php print render($title_prefix); ?>
@@ -76,7 +82,7 @@
   <?php print render($title_suffix); ?>
   <div class="search-snippet-info">
     <?php if ($result['node']->body['und'][0]['value']):?>
-      <div class="text -body-content"><?php echo $snippet . '...' ?></div>
+      <div class="text -body-content"><?php echo $snippet ?></div>
       <br>
       <p class="text -blue -bold"><?php echo $result['type'] ?></p>
     <?php endif; ?>
