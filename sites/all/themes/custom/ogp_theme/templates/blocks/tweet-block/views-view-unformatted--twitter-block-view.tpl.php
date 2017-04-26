d9af4dd8fc0d3ab4e4b1510f1af35fce80175ca0
Hector Vizzuality <hectorvizzuality@MacBook-Air-de-Hector.local>
2017-04-26 09:18:31 +0200

    add tweet link original

--- sites/all/themes/custom/ogp_theme/templates/blocks/tweet-block/views-view-unformatted--twitter-block-view.tpl.php
+++ sites/all/themes/custom/ogp_theme/templates/blocks/tweet-block/views-view-unformatted--twitter-block-view.tpl.php
@@ -3,9 +3,10 @@
     <span class="text -title-x-small -blue">Open Gov Partnership</span>
     <svg class="icon -medium"><use xlink:href="#icon-twitter"></use></svg>
   </a>
-  <?php foreach ($rows as $id => $row): ?>
-    <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
-      <?php print $row; ?>
-    </div>
-  <?php endforeach; ?>
+    <?php foreach ($rows as $id => $row): ?>
+      <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
+        <?php print $row; ?>
+      </div>
+    <?php endforeach; ?>
+    <a href="#" class="text -blue link-twitter" target="_blank">Go to original tweet</a>
 </div>
