248fc0b9eca2336ad9c9d8cd2250aa5299b707f3
Rodrigo Solís <rodrigo.solis@vizzuality.com>
2017-05-04 10:54:09 +0200

    replaces twitter module + changes styles

---
+++ sites/all/themes/custom/ogp_theme/templates/blocks/tweet-block/views-view-unformatted--tweet-feed.tpl.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+<div class="c-social-block -tweet">
+    <a href="https://twitter.com/opengovpart" target="_blank" class="contain-title-icon">
+        <span class="text -title-x-small -blue">Open Gov Partnership</span>
+        <svg class="icon -medium"><use xlink:href="#icon-twitter"></use></svg>
+    </a>
+    <?php foreach ($rows as $id => $row): ?>
+        <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
+            <?php print $row; ?>
+        </div>
+    <?php endforeach; ?>
+    <a href="#" class="text -blue link-twitter" target="_blank">Go to original tweet</a>
+</div>
