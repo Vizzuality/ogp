a6e2d9af5487fe14f7a235867503f4fe20072d83
Hector Vizzuality <hectorvizzuality@MacBook-Air-de-Hector.local>
2017-05-10 08:52:33 +0200

    styles people involved with zoom level

--- sites/all/themes/custom/ogp_theme/scripts/bundle.js
+++ sites/all/themes/custom/ogp_theme/scripts/bundle.js
@@ -1593,7 +1593,7 @@ function showCountriesPage() {
       },
       people: {
         sql: 'SELECT numberpeopleinvolved,country,countryid, pi.cartodb_id, st_centroid(wb.the_geom_webmercator) the_geom_webmercator FROM u4yhv_fq5_jb91rzuzgd8q pi INNER JOIN country_centroids_all wb on pi.country = wb.short_name WHERE wb.the_geom_webmercator IS NOT NULL',
-        cartocss: '#layer {marker-width: 50;marker-height: 45;marker-fill: #007acb;marker-fill-opacity: 1;marker-file:url("https://s3.amazonaws.com/com.cartodb.users-assets.production/maki-icons/marker-18.svg");marker-allow-overlap: true;marker-line-width: 1;marker-line-color: #4b392f;marker-line-opacity: 1;marker-comp-op: src-over;[zoom = 2] {marker-width: 30;marker-height: 25;}[zoom = 3] {marker-width: 35;marker-height: 30;}[zoom = 4] {marker-width: 45;marker-height: 40;} [zoom = 5] {marker-width: 50;marker-height: 45;}[zoom = 6] {marker-width: 55;marker-height: 50;}}',
+        cartocss: '#layer {marker-width: 50;marker-height: 45;marker-fill: #007acb;marker-fill-opacity: 1;marker-file:url("https://s3.amazonaws.com/com.cartodb.users-assets.production/maki-icons/marker-18.svg");marker-allow-overlap: true;marker-line-width: 1;marker-line-color: #4b392f;marker-line-opacity: 1;marker-comp-op: src-over;[zoom = 2] {marker-width: 15;marker-height: 12;}[zoom = 3] {marker-width: 20;marker-height: 17;}[zoom = 4] {marker-width: 30;marker-height: 27;}[zoom = 5] {marker-width: 40;marker-height: 35;}[zoom = 6] {marker-width: 45;marker-height: 40;}}',
         interactivity: 'numberpeopleinvolved,countryid, country, cartodb_id, the_geom_webmercator',
         name: 'people'
       },
